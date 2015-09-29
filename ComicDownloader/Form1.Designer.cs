0ad2d7a4dc37f5821a2b1a8dd293a54a20876270
cash <cash@cashwu.com>
2015-09-29 23:42:37 +0800

    Init

---
+++ ComicDownloader/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace ComicDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericFrom = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericTo = new System.Windows.Forms.NumericUpDown();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbComic = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericTo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(101, 38);
+            this.btnSelectPath.TabIndex = 0;
+            this.btnSelectPath.Text = "Select Path";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Location = new System.Drawing.Point(136, 25);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(0, 12);
+            this.lblPath.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select Comic ";
+            // 
+            // numericFrom
+            // 
+            this.numericFrom.Location = new System.Drawing.Point(14, 159);
+            this.numericFrom.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericFrom.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericFrom.Name = "numericFrom";
+            this.numericFrom.Size = new System.Drawing.Size(120, 22);
+            this.numericFrom.TabIndex = 5;
+            this.numericFrom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericFrom.ValueChanged += new System.EventHandler(this.numericFrom_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Vol From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Vol To";
+            // 
+            // numericTo
+            // 
+            this.numericTo.Location = new System.Drawing.Point(164, 159);
+            this.numericTo.Maximum = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            this.numericTo.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericTo.Name = "numericTo";
+            this.numericTo.Size = new System.Drawing.Size(120, 22);
+            this.numericTo.TabIndex = 7;
+            this.numericTo.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericTo.ValueChanged += new System.EventHandler(this.numericTo_ValueChanged);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(195, 197);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(89, 39);
+            this.btnStart.TabIndex = 9;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // cbComic
+            // 
+            this.cbComic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbComic.FormattingEnabled = true;
+            this.cbComic.Location = new System.Drawing.Point(12, 94);
+            this.cbComic.Name = "cbComic";
+            this.cbComic.Size = new System.Drawing.Size(121, 20);
+            this.cbComic.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(298, 252);
+            this.Controls.Add(this.cbComic);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericTo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPath);
+            this.Controls.Add(this.btnSelectPath);
+            this.Name = "Form1";
+            this.Text = "ComicDownloader";
+            ((System.ComponentModel.ISupportInitialize)(this.numericFrom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericTo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericTo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ComboBox cbComic;
+    }
+}
+
---
+++ ComicDownloader/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace ComicDownloader
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<int, string> _comicDic = new Dictionary<int, string>()
+        {
+            { 1698, "食戟之靈" },
+            { 1220, "美食的俘虜" },
+            { 3945, "火之丸相扑" },
+            { 1152, "海賊王" }
+        };
+
+        /// <summary>
+        /// {0} => comic name
+        /// {1} => vol number
+        /// {2} => page number
+        /// </summary>
+        private string _baseUri = @"http://web4.cartoonmad.com/c86es736r62/{0}/{1}/{2}.jpg";
+
+        /// <summary>
+        /// {0} => base path
+        /// {1} => comic name
+        /// {2} => vol number
+        /// {3} => page number
+        /// </summary>
+        private string _baseFileName = @"{0}\{1}\{2}\{3}.jpg";
+
+        private int _pageMaxNumber = 30;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+
+        private void Init()
+        {
+            cbComic.DisplayMember = "Name";
+            cbComic.ValueMember = "Id";
+
+            foreach (var item in _comicDic)
+            {
+                cbComic.Items.Add(new Comic { Name = item.Value, Id = item.Key });
+            }
+
+            cbComic.SelectedIndex = 0;
+
+            lblPath.Text = @"H:\comic";
+        }
+
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.ShowDialog();
+
+            lblPath.Text = folderBrowser.SelectedPath;
+        }
+
+        private void numericFrom_ValueChanged(object sender, EventArgs e)
+        {
+            numericTo.Minimum = numericFrom.Value;
+
+            if (numericFrom.Value > numericTo.Value)
+            {
+                numericTo.Value = numericFrom.Value;
+            }
+        }
+
+        private void numericTo_ValueChanged(object sender, EventArgs e)
+        {
+            numericFrom.Maximum = numericTo.Value;
+
+            if (numericFrom.Value > numericTo.Value)
+            {
+                numericFrom.Value = numericTo.Value;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(lblPath.Text))
+            {
+                MessageBox.Show("Please select path !!");
+
+                return;
+            }
+
+            var comicItem = cbComic.Items[cbComic.SelectedIndex] as Comic;
+
+            this.Download(comicItem);
+        }
+
+        private void Download(Comic comic)
+        {
+            var downloadDic = this.GetDownloadUri(comic);
+
+            var dlUri = string.Empty;
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    foreach (var dic in downloadDic)
+                    {
+                        dlUri = dic.Key;
+                        var uri = new Uri(dic.Key);
+
+                        //client.DownloadFile(uri, dic.Value);
+
+                        if (CheckFile(dic.Key))
+                        {
+                            client.DownloadFile(uri, dic.Value);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("DL Uri .." + dlUri);
+                Console.WriteLine(e);
+            }
+
+            MessageBox.Show("done..");
+        }
+
+        private Dictionary<string, string> GetDownloadUri(Comic comic)
+        {
+            var result = new Dictionary<string, string>();
+
+            var minVol = numericFrom.Value;
+            var maxVol = numericTo.Value;
+
+            for (; minVol <= maxVol; minVol++)
+            {
+                for (int pageNumber = 1; pageNumber <= _pageMaxNumber; pageNumber++)
+                {
+                    var fixPageNumber = this.FixPageNumber(pageNumber);
+
+                    var dlUri = string.Format(_baseUri, comic.Id, minVol, fixPageNumber);
+
+                    var fileName = string.Format(_baseFileName, lblPath.Text, comic.Name, minVol, fixPageNumber);
+
+                    this.CheckPath(fileName);
+
+                    result.Add(dlUri, fileName);
+                }
+            }
+
+            return result;
+        }
+
+        private void CheckPath(string fileName)
+        {
+            var path = Path.GetDirectoryName(fileName);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private string FixPageNumber(int pageNumber)
+        {
+            return pageNumber.ToString().PadLeft(3, '0');
+        }
+
+        private bool CheckFile(string url)
+        {
+            HttpWebResponse response = null;
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "HEAD";
+
+            var isFileExist = true;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception ex)
+            {
+                isFileExist = false;
+
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    isFileExist = response.StatusCode == HttpStatusCode.OK;
+                    response.Close();
+                }
+            }
+
+            return isFileExist;
+        }
+    }
+
+    class Comic
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+    }
+}
